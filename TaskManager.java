00c32b0450070f0d33c411d80eecd446f6dab7e6
Acrobat622 <86.ed.liu@gmail.com>
2015-02-05 13:46:11 +0800

    Only Skeleton files, do not push or commit to the main repo. Please create one repo of your own and make a copy of this to your repo

---
+++ TaskManager.java
@@ -0,0 +1,13 @@
+class TaskManager	{
+	private ArrayList<TaskData> uncompletedTasks;
+	private ArrayList<TaskData> completedTasks;
+	
+	public TaskManager()	{
+		
+	}
+	
+	public 
+	public static void main (String[] args)	{
+		
+	}
+}
