a0d78140b31acdcdd7ac123051793f54feac3e9d
Aw Wei Lin <weinilinhappy@gmail.com>
2015-02-27 23:24:01 +0800

    extract time 12 am and 12am

--- src/parser/TimeParser.java
+++ src/parser/TimeParser.java
@@ -1,5 +1,23 @@
-package parser;
+package src.parser;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TimeParser {
+	
+	public TimeParser(){
+		
+	}
+	
+	public String extractTime(String userInput){
+		Pattern timeDetector = 
+				Pattern.compile("(\\b\\d+[.:,-]\\d+|\\d+)(am|pm| am| pm)\\b");
+		Matcher matchedWithTime = timeDetector.matcher(listOfTaskData);
 
+       storageOfTimePerTasks = new ArrayList<String>();
+
+		
+        while (matchedWithTime.find()) {
+            storageOfTimePerTasks.add(matchedWithTime.group());      
+        }
+	}
 }
