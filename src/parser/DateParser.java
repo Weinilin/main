f5aad21623b187d393c642c2a605a92df30d0a2f
Aw Wei Lin <weinilinhappy@gmail.com>
2015-02-27 23:19:53 +0800

    extract date in format like 24Dec2014, 24.02.2015, 24/2/2015

--- src/parser/DateParser.java
+++ src/parser/DateParser.java
@@ -1,5 +1,25 @@
 package parser;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DateParser {
+
+	private static final String  date_Keyword = "\\d+((\\s|\\S)\\w{3,9}(\\s|\\S)|[/.]\\d{2}[/.])\\d+";
+
+	public DateParser(){
+
+	}
+
+	public String extractDate(String userInput){
+		Pattern dateDetector = Pattern.compile(date_Keyword);
+		Matcher containDate = dateDetector.matcher(
+				userInput);
+
+		while(matchedWithlistOfTaskDataWithoutTime.find()){
+			String dateOfTheTask = matchedWithlistOfTaskDataWithoutTime.group();
+		}
+
+		return dateofTheTask;
+	}
 
 }
