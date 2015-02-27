2d48302f3add5b25378aabed771c549805b44fb7
Aw Wei Lin <weinilinhappy@gmail.com>
2015-02-27 23:22:32 +0800

    determine taskType by number of time store in the array list

--- src/parser/TaskTypeParser.java
+++ src/parser/TaskTypeParser.java
@@ -2,4 +2,26 @@ package parser;
 
 public class TaskTypeParser {
 
+	public String extractTaskType(ArrayList<String> storageOfTime) {
+		int numberOfTimeInput = getNumberOfTimeInput(storageOfTime);
+		String taskType;
+
+		if(numberOfTimeInput == 2){
+			taskType = "timedTask";
+		}
+		else if(numberOfTimeInput == 1){
+			taskType = "deadlinesTask";
+		}
+		else{
+			taskType = "floatingTask";
+		}
+		return taskType;
+	}
+
+	private int getNumberOfTimeInput(ArrayList<String> storageOfTime) {
+		int numberOfTimeInput = storageOfTime.size();
+		return numberOfTimeInput;
+	}
+
 }
+
