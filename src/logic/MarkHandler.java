ece1f937e83aa72935691a67839b2aa8076832d8
Yong Xien <yxchng@120-245.priv23.nus.edu.sg>
2015-03-07 17:56:45 +0800

    Updated MarkHandler

--- src/logic/MarkHandler.java
+++ src/logic/MarkHandler.java
@@ -1,5 +1,27 @@
 package logic;
 
-public class MarkHandler {
+import java.util.ArrayList;
 
+import parser.IndexParser;
+import database.Memory;
+
+public class MarkHandler {
+	
+	private Memory memory;
+	
+	public MarkHandler(Memory memory) {
+		this.memory = memory;
+	}
+	
+	protected boolean markTaskDone(String taskInformation) throws IndexOutOfBoundsException  {
+		IndexParser ip = new IndexParser();
+		int index = ip.extractIndex(taskInformation);
+		try {
+			memory.getTaskList().get(index).setStatus("done");
+		} catch (IndexOutOfBoundsException iob) {
+			return false;
+		} 
+	
+		return true;
+	}
 }
