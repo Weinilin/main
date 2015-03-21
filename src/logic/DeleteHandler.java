7974adff3cb09912d1540eca605d09e476498b95
Acrobat622 <86.ed.liu@gmail.com>
2015-03-21 18:25:43 +0800

    refactorizing

--- src/logic/DeleteHandler.java
+++ src/logic/DeleteHandler.java
@@ -40,12 +40,11 @@ public class DeleteHandler extends CommandHandler {
 		deleteLogger.entering(getClass().getName(), "preparing for delete");
 
 		String[] token = parameter.split(" ");
-		if (token[0].toLowerCase().trim().equals("help") ||
-				parameter.trim().equals("")) {
+		if (isHelp(token) || isEmptyParameter(parameter)) {
 			return getHelp();
 		}
 		
-		if (token[0].toLowerCase().trim().equals("all")) {
+		if (isAll(token)) {
 			ClearHandler clrHandler = new ClearHandler();
 			return clrHandler.execute(token[0], "", taskList);
 		}
@@ -61,22 +60,75 @@ public class DeleteHandler extends CommandHandler {
 			int index = ip.getIndex() - 1;
 			try {
 				removedTask.add(taskList.get(index));				
-				goodFeedback += t + " ";
+				goodFeedback = appendFeedback(goodFeedback, t);
 				deleteLogger.log(Level.FINE, "Removed " + removedTask.toString() + "\n");
 			} catch (IndexOutOfBoundsException iob) {
-				badFeedback += t + " ";
+				badFeedback = appendFeedback(badFeedback, t);
 				deleteLogger.log(Level.WARNING, t + " is invalid!\n", iob);			
 			} 
 		}
 		
-		for (Task task: removedTask) {
-			taskList.remove(task);
-			memory.removeTask(task);
-		}
+		deleteTasks(taskList, removedTask);
 		
 		String feedback = "Removed tasks " + goodFeedback + "\n" +
 						  "Invalid input " + badFeedback + "\n";	
 		return feedback;
+	}
+
+
+	/**
+	 * delete the tasks in taskList and memory
+	 * @param taskList
+	 * @param removedTask
+	 */
+	private void deleteTasks(ArrayList<Task> taskList,
+			ArrayList<Task> removedTask) {
+		for (Task task: removedTask) {
+			taskList.remove(task);
+			memory.removeTask(task);
+		}
+	}
+
+
+	/**
+	 * append the indexes for valid deletion or invalid input
+	 * @param goodFeedback
+	 * @param index
+	 * @return - feedback string
+	 */
+	private String appendFeedback(String feedback, String index) {
+		feedback += index + " ";
+		return feedback;
+	}
+
+
+	/**
+	 * decide whether is delete all
+	 * @param token
+	 * @return
+	 */
+	private boolean isAll(String[] token) {
+		return token[0].toLowerCase().trim().equals("all");
+	}
+
+
+	/**
+	 * check if the argument user typed is empty
+	 * @param parameter
+	 * @return
+	 */
+	private boolean isEmptyParameter(String parameter) {
+		return parameter.trim().equals("");
+	}
+
+
+	/**
+	 * check if user is looking for help
+	 * @param token
+	 * @return
+	 */
+	private boolean isHelp(String[] token) {
+		return token[0].toLowerCase().trim().equals("help");
 	}
 
 	@Override
