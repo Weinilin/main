3529dd64fce1dc9786201644a63b7ce27e7b18ac
Yong Xien <yxchng@r-43-177-28-172.comp.nus.edu.sg>
2015-03-23 10:21:34 +0800

    Change the format of the table printed in TaskListPrinter

--- src/ui/TaskListUI.java
+++ src/ui/TaskListUI.java
@@ -14,39 +14,60 @@ public class TaskListUI {
 	
 	public void showTask() {
 		TaskPrinter tp = new TaskPrinter();
+
+		
 		ArrayList<ArrayList<String>> taskListMatrix1 = new ArrayList<ArrayList<String>>();
 		ArrayList<ArrayList<String>> taskListMatrix2 = new ArrayList<ArrayList<String>>();
 
 		
-		ArrayList<String> dataFields = new ArrayList<String>();
-		dataFields.add("No.");
-		dataFields.add("Description");
-		dataFields.add("Time");
-		dataFields.add("Status");
-		taskListMatrix1.add(dataFields);
-		taskListMatrix2.add(dataFields);
+		ArrayList<String> dataFields1 = new ArrayList<String>();
+		ArrayList<String> dataFields2 = new ArrayList<String>();
+
+		dataFields1.add("No.");
+		dataFields1.add("Description");
+		dataFields1.add("Time");
+		dataFields1.add("Status");
+		
+		dataFields2.add("No.");
+		dataFields2.add("Description");
+		dataFields2.add("Status");
+		
+		taskListMatrix1.add(dataFields1);
+		taskListMatrix2.add(dataFields2);
 
 		int taskNumber = 1;
-		
+
 		for (Task task : taskList) {
-			ArrayList<String> taskInformation = new ArrayList<String>();
-			taskInformation.add((Integer.toString(taskNumber++)));
-			taskInformation.add(task.getDescription());
-			taskInformation.add(task.getDateTime());
-			taskInformation.add(task.getStatus());
-			
 			if (task.getTaskType().equals("floating task")) {
+				ArrayList<String> taskInformation = new ArrayList<String>();
+				taskInformation.add((Integer.toString(taskNumber++)));
+				taskInformation.add(task.getDescription());
+				taskInformation.add(task.getStatus());
 				taskListMatrix2.add(taskInformation);
 			} else {
+				ArrayList<String> taskInformation = new ArrayList<String>();
+				taskInformation.add((Integer.toString(taskNumber++)));
+				taskInformation.add(task.getDescription());
+				taskInformation.add(task.getDateTime());
+				taskInformation.add(task.getStatus());
 				taskListMatrix1.add(taskInformation);
 			}
 		}
-		
-		tp.print(taskListMatrix1);
-		if (taskListMatrix2.size() > 1) {
-			System.out.println("Floating task");
-		}
-		tp.print(taskListMatrix2);
 
+		if (taskListMatrix1.size() > 1) {
+			System.out.println("Time Tasks and Deadlines");
+			tp.printTasksWithTime(taskListMatrix1);
+			System.out.println();
+
+
+		}
+		
+		if (taskListMatrix2.size() > 1) {
+			System.out.println("Floating Task");
+			tp.printFloatingTask(taskListMatrix2);
+	        System.out.println();
+
+		}
+		
 	}
 }
