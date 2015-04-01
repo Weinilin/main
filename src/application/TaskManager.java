4eca2285ad6ba4e72b15f74477539128260ac6fa
Yong Xien <yxchng@203-113.priv23.nus.edu.sg>
2015-04-01 23:04:54 +0800

    change main and processuserinput method

--- src/application/TaskManager.java
+++ src/application/TaskManager.java
@@ -1,14 +1,35 @@
 
 package application;
 
+import java.util.Scanner;
+
 import ui.CommandLineInterface;
+import ui.GUI;
 
 class TaskManager	{
+    
+    private static final String COMMAND_MESSAGE = new String("Command: ");
+    private static final String WELCOME_MESSAGE = new String( "Welcome to TaskManager!\n");
 	
-	public static void main (String[] args)	{		
+	public static void main (String[] args)	{	
 		CommandLineInterface cli = new CommandLineInterface();
-		cli.processUserInput();
+		Scanner scanner = new Scanner(System.in);
+		
+		GUI std = new GUI();
+        std.run();
+		
+	    printMessageToUser(String.format(WELCOME_MESSAGE));
+
+		while (true) {
+		    printMessageToUser(String.format(COMMAND_MESSAGE));
+		    String userInput = scanner.nextLine();
+		    String feedback = cli.processUserInput(userInput);
+	        printMessageToUser(feedback);
+		}
 	}
 	
+	public static void printMessageToUser(String message){
+        System.out.println(message);
+    }
 
 }
--- src/ui/CommandLineInterface.java
+++ src/ui/CommandLineInterface.java
@@ -9,7 +9,6 @@ public class CommandLineInterface {
 	private static final String COMMAND_MESSAGE = new String("Command: ");
 	private static final String WELCOME_MESSAGE = new String( "Welcome to TaskManager!\n");
 	private static final String GOODBYE_MESSAGE = new String("GoodBye!\n");
-	private Scanner scanner;
 	
 
 
@@ -19,31 +18,22 @@ public class CommandLineInterface {
 	/**
 	 * Scan the user input and execute the command.
 	 */
-	public void processUserInput(){
-		String userCommand, message;
-		scanner = new Scanner(System.in);
+	public String processUserInput(String userInput){
+		String message;
 		LogicController commandHandler = LogicController.getInstance();
 		
 		TaskListUI taskListUI = new TaskListUI(commandHandler.getTaskList());
 		taskListUI.showTask();
        
 
-		GUI std = new GUI();
-	    std.run();
-		printMessageToUser(String.format(WELCOME_MESSAGE));
+		
 
-		while (true) {
-			printMessageToUser(String.format(COMMAND_MESSAGE));
-			userCommand = scanner.nextLine();
-			message = commandHandler.executeCommand(userCommand);
-			if (message == null) {
-				printMessageToUser(GOODBYE_MESSAGE);
-				System.exit(0);
-			}			
-			printMessageToUser(message);
-			std.updateTable();
-			taskListUI.showTask();
-		}
+	    message = commandHandler.executeCommand(userInput);
+						
+		std.updateTable();
+		taskListUI.showTask();
+		
+		return message;
 	}
 
 
@@ -57,10 +47,7 @@ public class CommandLineInterface {
 	    printMessageToUser(String.format(COMMAND_MESSAGE));
 
 	    message = commandHandler.executeCommand(userInput);
-	    if (message == null) {
-	        printMessageToUser(GOODBYE_MESSAGE);
-	        System.exit(0);
-	    }         
+	            
 	    
 	    TaskListUI taskListUI = new TaskListUI(commandHandler.getTaskList());
 	    taskListUI.showTask();
@@ -73,9 +60,10 @@ public class CommandLineInterface {
 	 * Print all of the message to the user
 	 * @param message
 	 */
-	public void printMessageToUser(String message){
-		System.out.println(message);
-	}
+	
+	public static void printMessageToUser(String message){
+        System.out.println(message);
+    }
 
 	
 	
