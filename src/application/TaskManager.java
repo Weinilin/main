f5d1aaa6cbcc50966158d8751ba5a3e7b1b32210
Aw Wei Lin <weinilinhappy@gmail.com>
2015-04-01 23:43:37 +0800

    accidentally delete taskManager

---
+++ src/application/TaskManager.java
@@ -0,0 +1,12 @@
+package application;
+
+import ui.CommandLineInterface;
+
+class TaskManager   {
+    
+    public static void main (String[] args) {       
+        CommandLineInterface cli = new CommandLineInterface();
+        cli.processUserInput();
+    }
+
+}
